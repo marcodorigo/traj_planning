623c35c99e37eb43e0023a7320f5edbcf3f7bc19
marcodorigo <marco3.dorigo@mail.polimi.it>
2025-10-03 10:06:38 +0200

    ignore this

--- traj_planning/moveit_collision_aware_planner.py
+++ traj_planning/moveit_collision_aware_planner.py
@@ -126,3 +126,395 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+# [global_config]
+#   window_state = fullscreen
+#   suppress_multiple_term_dialog = True
+# [keybindings]
+# [profiles]
+#   [[default]]
+#     cursor_color = "#aaaaaa"
+# [layouts]
+#   [[default]]
+#     [[[child0]]]
+#       type = Window
+#       parent = ""
+#       order = 0
+#       position = 26:23
+#       maximised = False
+#       fullscreen = False
+#       size = 734, 451
+#       title = marco@marco: ~/UR5_ws
+#       last_active_term = c2fe6aab-4913-4b67-9417-5998be49c196
+#       last_active_window = True
+#     [[[terminal1]]]
+#       type = Terminal
+#       parent = child0
+#       order = 0
+#       profile = default
+#       uuid = c2fe6aab-4913-4b67-9417-5998be49c196
+#       command = ""
+#   [[rviz]]
+#     [[[child0]]]
+#       type = Window
+#       parent = ""
+#       order = 0
+#       position = 26:23
+#       maximised = False
+#       fullscreen = False
+#       size = 734, 451
+#       title = marco@marco: ~/UR5_ws
+#       last_active_term = c2fe6aab-4913-4b67-9417-5998be49c196
+#       last_active_window = True
+#     [[[child1]]]
+#       type = HPaned
+#       parent = child0
+#       order = 0
+#       position = 364
+#       ratio = 0.4993141289437586
+#     [[[terminal2]]]
+#       type = Terminal
+#       parent = child1
+#       order = 0
+#       profile = default
+#       uuid = c2fe6aab-4913-4b67-9417-5998be49c196
+#       command = '''bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch ur5e_bringup fake_start.launch.py initial_joint_controller:="forward_velocity_controller"; exec bash''''
+#     [[[terminal3]]]
+#       type = Terminal
+#       parent = child1
+#       order = 1
+#       profile = default
+#       uuid = a90026bd-0777-4875-a371-f37c3eaa2e68
+#       command = ""
+#   [[wii]]
+#     [[[child0]]]
+#       type = Window
+#       parent = ""
+#       order = 0
+#       position = 26:23
+#       maximised = False
+#       fullscreen = False
+#       size = 734, 451
+#       title = marco@marco: ~
+#       last_active_term = 6766d1e0-7237-4333-9000-9d989db245a7
+#       last_active_window = True
+#     [[[child1]]]
+#       type = HPaned
+#       parent = child0
+#       order = 0
+#       position = 364
+#       ratio = 0.4993141289437586
+#     [[[child2]]]
+#       type = VPaned
+#       parent = child1
+#       order = 0
+#       position = 223
+#       ratio = 0.5
+#     [[[terminal3]]]
+#       type = Terminal
+#       parent = child2
+#       order = 0
+#       profile = default
+#       uuid = 6766d1e0-7237-4333-9000-9d989db245a7
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch compliance_controller admittance_controller.launch.py virtual_wrench_off:=false measured_torque_off:=true use_dummy_force_sensor:=true; exec bash'"
+#     [[[child4]]]
+#       type = VPaned
+#       parent = child2
+#       order = 1
+#       position = 109
+#       ratio = 0.5
+#     [[[terminal5]]]
+#       type = Terminal
+#       parent = child4
+#       order = 0
+#       profile = default
+#       uuid = 461a8ca4-dc6d-4256-b6f2-029563474a32
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch differential_gt differential_gt_sim.launch.py; exec bash'"
+#     [[[child6]]]
+#       type = VPaned
+#       parent = child4
+#       order = 1
+#       position = 52
+#       ratio = 0.5
+#     [[[terminal7]]]
+#       type = Terminal
+#       parent = child6
+#       order = 0
+#       profile = default
+#       uuid = 5daa9541-ce8d-45bb-8be1-c184f3bad8ca
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run differential_gt differential_gt; exec bash'"
+#     [[[terminal8]]]
+#       type = Terminal
+#       parent = child6
+#       order = 1
+#       profile = default
+#       uuid = d26c4143-5da5-440e-82a7-5c995e585cd9
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch traj_planning traj_planning_launch.py use_wii_controller:=true; exec bash'"
+#     [[[child9]]]
+#       type = VPaned
+#       parent = child1
+#       order = 1
+#       position = 223
+#       ratio = 0.5
+#     [[[terminal10]]]
+#       type = Terminal
+#       parent = child9
+#       order = 0
+#       profile = default
+#       uuid = 986d3859-1887-4eb3-b6ac-5a0d74612e92
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch joy_to_wrench joystick_launch.py; exec bash'"
+#     [[[child11]]]
+#       type = VPaned
+#       parent = child9
+#       order = 1
+#       position = 109
+#       ratio = 0.5
+#     [[[terminal12]]]
+#       type = Terminal
+#       parent = child11
+#       order = 0
+#       profile = default
+#       uuid = 5ba378de-1f30-488e-b773-309402a281da
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch fuzzy_inference_system fuzzy_inference_system_launch.py; exec bash'"
+#     [[[child13]]]
+#       type = VPaned
+#       parent = child11
+#       order = 1
+#       position = 52
+#       ratio = 0.5
+#     [[[terminal14]]]
+#       type = Terminal
+#       parent = child13
+#       order = 0
+#       profile = default
+#       uuid = 685de651-9fbe-42c5-a49e-ae2ff1ff8f9d
+#       command = '''bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch ur5e_bringup fake_start.launch.py initial_joint_controller:="forward_velocity_controller"; exec bash''''
+#     [[[terminal15]]]
+#       type = Terminal
+#       parent = child13
+#       order = 1
+#       profile = default
+#       uuid = bc9679d1-746e-457e-a632-3fa3098a6f52
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash; exec bash'"
+#   [[falcon]]
+#     [[[child0]]]
+#       type = Window
+#       parent = ""
+#       order = 0
+#       position = 26:23
+#       maximised = False
+#       fullscreen = False
+#       size = 734, 451
+#       title = marco@marco: ~
+#       last_active_term = 986d3859-1887-4eb3-b6ac-5a0d74612e92
+#       last_active_window = True
+#     [[[child1]]]
+#       type = HPaned
+#       parent = child0
+#       order = 0
+#       position = 364
+#       ratio = 0.4993141289437586
+#     [[[child2]]]
+#       type = VPaned
+#       parent = child1
+#       order = 0
+#       position = 223
+#       ratio = 0.5
+#     [[[terminal3]]]
+#       type = Terminal
+#       parent = child2
+#       order = 0
+#       profile = default
+#       uuid = 6766d1e0-7237-4333-9000-9d989db245a7
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch compliance_controller admittance_controller.launch.py virtual_wrench_off:=false measured_torque_off:=true use_dummy_force_sensor:=true; exec bash'"
+#     [[[child4]]]
+#       type = VPaned
+#       parent = child2
+#       order = 1
+#       position = 109
+#       ratio = 0.5
+#     [[[terminal5]]]
+#       type = Terminal
+#       parent = child4
+#       order = 0
+#       profile = default
+#       uuid = 461a8ca4-dc6d-4256-b6f2-029563474a32
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch differential_gt differential_gt_sim.launch.py; exec bash'"
+#     [[[child6]]]
+#       type = VPaned
+#       parent = child4
+#       order = 1
+#       position = 52
+#       ratio = 0.5
+#     [[[terminal7]]]
+#       type = Terminal
+#       parent = child6
+#       order = 0
+#       profile = default
+#       uuid = 5daa9541-ce8d-45bb-8be1-c184f3bad8ca
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run differential_gt differential_gt; exec bash'"
+#     [[[terminal8]]]
+#       type = Terminal
+#       parent = child6
+#       order = 1
+#       profile = default
+#       uuid = d26c4143-5da5-440e-82a7-5c995e585cd9
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch traj_planning traj_planning_launch.py; exec bash'"
+#     [[[child9]]]
+#       type = VPaned
+#       parent = child1
+#       order = 1
+#       position = 223
+#       ratio = 0.5
+#     [[[terminal10]]]
+#       type = Terminal
+#       parent = child9
+#       order = 0
+#       profile = default
+#       uuid = 986d3859-1887-4eb3-b6ac-5a0d74612e92
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run falcon master; exec bash'"
+#     [[[child11]]]
+#       type = VPaned
+#       parent = child9
+#       order = 1
+#       position = 109
+#       ratio = 0.5
+#     [[[terminal12]]]
+#       type = Terminal
+#       parent = child11
+#       order = 0
+#       profile = default
+#       uuid = 5ba378de-1f30-488e-b773-309402a281da
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run falcon falcon_joystick; exec bash'"
+#     [[[child13]]]
+#       type = VPaned
+#       parent = child11
+#       order = 1
+#       position = 52
+#       ratio = 0.5
+#     [[[terminal14]]]
+#       type = Terminal
+#       parent = child13
+#       order = 0
+#       profile = default
+#       uuid = 685de651-9fbe-42c5-a49e-ae2ff1ff8f9d
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch fuzzy_inference_system fuzzy_inference_system_launch.py; exec bash'"
+#     [[[terminal15]]]
+#       type = Terminal
+#       parent = child13
+#       order = 1
+#       profile = default
+#       uuid = bc9679d1-746e-457e-a632-3fa3098a6f52
+#       command = '''bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch ur5e_bringup fake_start.launch.py initial_joint_controller:="forward_velocity_controller"; exec bash''''
+#   [[real]]
+#     [[[child0]]]
+#       type = Window
+#       parent = ""
+#       order = 0
+#       position = 0:0
+#       maximised = True
+#       fullscreen = False
+#       size = 1010, 656
+#       title = marco@marco: ~
+#       last_active_term = 6766d1e0-7237-4333-9000-9d989db245a7
+#       last_active_window = False
+#     [[[child1]]]
+#       type = HPaned
+#       parent = child0
+#       order = 0
+#       position = 502
+#       ratio = 0.49950248756218907
+#     [[[child2]]]
+#       type = VPaned
+#       parent = child1
+#       order = 0
+#       position = 326
+#       ratio = 0.500768049155146
+#     [[[terminal3]]]
+#       type = Terminal
+#       parent = child2
+#       order = 0
+#       profile = default
+#       uuid = 6766d1e0-7237-4333-9000-9d989db245a7
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch ur5e_bringup real_start.launch.py initial_joint_controller:=forward_velocity_controller; exec bash'"
+#     [[[child4]]]
+#       type = VPaned
+#       parent = child2
+#       order = 1
+#       position = 160
+#       ratio = 0.5
+#     [[[terminal5]]]
+#       type = Terminal
+#       parent = child4
+#       order = 0
+#       profile = default
+#       uuid = 461a8ca4-dc6d-4256-b6f2-029563474a32
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch compliance_controller admittance_controller.launch.py virtual_wrench_off:=false measured_torque_off:=true use_dummy_force_sensor:=true; exec bash'"
+#     [[[child6]]]
+#       type = VPaned
+#       parent = child4
+#       order = 1
+#       position = 78
+#       ratio = 0.5032258064516129
+#     [[[terminal7]]]
+#       type = Terminal
+#       parent = child6
+#       order = 0
+#       profile = default
+#       uuid = 5daa9541-ce8d-45bb-8be1-c184f3bad8ca
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch differential_gt differential_gt_sim.launch.py; exec bash'"
+#     [[[terminal8]]]
+#       type = Terminal
+#       parent = child6
+#       order = 1
+#       profile = default
+#       uuid = d26c4143-5da5-440e-82a7-5c995e585cd9
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run differential_gt differential_gt; exec bash'"
+#     [[[child9]]]
+#       type = VPaned
+#       parent = child1
+#       order = 1
+#       position = 326
+#       ratio = 0.500768049155146
+#     [[[terminal10]]]
+#       type = Terminal
+#       parent = child9
+#       order = 0
+#       profile = default
+#       uuid = 986d3859-1887-4eb3-b6ac-5a0d74612e92
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch traj_planning traj_planning_launch.py; exec bash'"
+#     [[[child11]]]
+#       type = VPaned
+#       parent = child9
+#       order = 1
+#       position = 160
+#       ratio = 0.5
+#     [[[terminal12]]]
+#       type = Terminal
+#       parent = child11
+#       order = 0
+#       profile = default
+#       uuid = 5ba378de-1f30-488e-b773-309402a281da
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run falcon master; exec bash'"
+#     [[[child13]]]
+#       type = VPaned
+#       parent = child11
+#       order = 1
+#       position = 78
+#       ratio = 0.5032258064516129
+#     [[[terminal14]]]
+#       type = Terminal
+#       parent = child13
+#       order = 0
+#       profile = default
+#       uuid = 685de651-9fbe-42c5-a49e-ae2ff1ff8f9d
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 run falcon falcon_joystick; exec bash'"
+#     [[[terminal15]]]
+#       type = Terminal
+#       parent = child13
+#       order = 1
+#       profile = default
+#       uuid = bc9679d1-746e-457e-a632-3fa3098a6f52
+#       command = "bash -c 'cd UR5_ws/ && source install/setup.bash && ros2 launch fuzzy_inference_system fuzzy_inference_system_launch.py; exec bash'"
+# [plugins]
