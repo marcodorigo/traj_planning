f72cccb2811db31e8d9e5eb2ac17d46c9b601c56
marcodorigo <10731784@polimi.it>
2025-10-07 16:40:10 +0200

    removed vsualization file (moved to common_config)

--- launch/traj_planning_launch.py
+++ launch/traj_planning_launch.py
@@ -27,12 +27,6 @@ def generate_launch_description():
         ),
         Node(
             package='traj_planning',
-            executable='visualizer_node',
-            name='visualizer_node',
-            output='screen',
-        ),
-        Node(
-            package='traj_planning',
             executable='HO_reference_finder',
             name='HO_reference_finder',
             output='screen',
@@ -41,12 +35,6 @@ def generate_launch_description():
             package='traj_planning',
             executable='ACS_reference_finder',
             name='ACS_reference_finder',
-            output='screen',
-        ),
-        Node(
-            package='common_config',
-            executable='parameter_publisher_node',
-            name='parameter_publisher_node',
             output='screen',
         ),
     ])
